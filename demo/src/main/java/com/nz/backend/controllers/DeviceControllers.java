1ddd179d0035db8bdf6fdaf96f4cc2ea16bf3b3d
wailoon <yongwl124578@gmail.com>
2025-03-11 17:44:51 +0800

    11/3

--- demo/src/main/java/com/nz/backend/controllers/DeviceControllers.java
+++ demo/src/main/java/com/nz/backend/controllers/DeviceControllers.java
@@ -1,13 +1,45 @@
 package com.nz.backend.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.nz.backend.services.JwtService;
 
 @RestController
 @RequestMapping("/api")
 @CrossOrigin(origins = "http://localhost:3000")
 public class DeviceControllers {
-    
+
+    @Autowired
+    private JwtService jwtService;
+
+    @Value("${jwt.secret.key}")
+    private String secretKey;
+
+    @Value("${device.serial}")
+    private String serialCode;
+
+    @GetMapping("/getDeviceDetails")
+    public ResponseEntity<?> getDeviceDetails() {
+        return null;
+    }
+
+    @PostMapping("/addDeviceToRoom")
+    public ResponseEntity<?> addDeviceToRoom() {
+        return null;
+    }
+
+    @DeleteMapping("/deleteDeviceFromRoom")
+    public ResponseEntity<?> dltDeviceFromRoom() {
+        return null;
+    }
+
     
 }
