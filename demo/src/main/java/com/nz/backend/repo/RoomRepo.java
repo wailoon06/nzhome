d52317baac53ce555501d98e2b257c13b31b308f
Kah Hin Lee <kahhin03@gmail.com>
2025-03-16 14:26:22 +0800

    room create, delete, update - done

--- demo/src/main/java/com/nz/backend/repo/RoomRepo.java
+++ demo/src/main/java/com/nz/backend/repo/RoomRepo.java
@@ -2,8 +2,11 @@ package com.nz.backend.repo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.nz.backend.entities.Family;
 import com.nz.backend.entities.Room;
 
 public interface RoomRepo extends JpaRepository<Room, Long>{
     Room findByRoomName (String roomName);
+
+    Room findByRoomNameAndFamilyName (String roomName, Family familyName);
 }
