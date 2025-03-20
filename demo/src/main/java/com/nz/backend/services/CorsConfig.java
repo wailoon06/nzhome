67ed8451db49826baa2123e34e22a8b5649f01d6
wailoon <yongwl124578@gmail.com>
2025-03-20 23:41:10 +0800

    cors

--- demo/src/main/java/com/nz/backend/services/CorsConfig.java
+++ demo/src/main/java/com/nz/backend/services/CorsConfig.java
@@ -13,7 +13,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("https://humdrum-beef-production.up.railway.app") // Replace with your Vercel URL
+                        .allowedOrigins("https://nzhome.vercel.app/") // Replace with your Vercel URL
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
