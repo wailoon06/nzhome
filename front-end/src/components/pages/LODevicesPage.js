3c19e17390b1c85f02b5bfdbe92f6bc10979fea5
Kah Hin Lee <kahhin03@gmail.com>
2025-03-24 21:11:19 +0800

    loading devices UI - design 1

--- front-end/src/components/pages/LODevicesPage.js
+++ front-end/src/components/pages/LODevicesPage.js
@@ -324,8 +324,12 @@ function LODevicesPage() {
               </div>
 
               {loading && (
-                <div className="text-center py-8">
-                  <p>Loading devices...</p>
+                // <div className="text-center py-8">
+                //   <p>Loading devices...</p>
+                // </div>
+                <div className="flex flex-col items-center justify-center py-12">
+                <div className="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-teal-500 mb-4"></div>
+                  <p className="text-lg text-gray-600">Loading devices...</p>
                 </div>
               )}
 
